7ea73852e93517c16870c9744e493a8acbde67ce
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-10-12 15:58:53 -0300

    feat: adicionado drawer para navegação entre as activities

---
+++ app/src/main/java/com/meicansoftware/consultamedica/NavigationTwo.java
@@ -0,0 +1,14 @@
+package com.meicansoftware.consultamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavigationTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
