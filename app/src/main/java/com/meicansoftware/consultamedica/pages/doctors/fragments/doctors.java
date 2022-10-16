41431dcf927515ad44764377328da304e8cd7bee
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-10-16 16:20:26 -0300

    feat: adicionado gridview para médicos

--- app/src/main/java/com/meicansoftware/consultamedica/pages/doctors/fragments/doctors.java
+++ app/src/main/java/com/meicansoftware/consultamedica/pages/doctors/fragments/doctors.java
@@ -10,9 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridView;
 
 import com.meicansoftware.consultamedica.R;
+import com.meicansoftware.consultamedica.config.ContatoMedicoDatabase;
+import com.meicansoftware.consultamedica.models.Doctor;
+import com.meicansoftware.consultamedica.models.Speciality;
 import com.meicansoftware.consultamedica.pages.specialities.fragments.specialities;
+import com.meicansoftware.consultamedica.pages.specialities.fragments.specialityAdapter;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,14 +28,7 @@ import com.meicansoftware.consultamedica.pages.specialities.fragments.specialiti
  */
 public class doctors extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ContatoMedicoDatabase db;
 
     public doctors() {
         // Required empty public constructor
@@ -45,20 +45,15 @@ public class doctors extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static doctors newInstance(String param1, String param2) {
         doctors fragment = new doctors();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        db = ContatoMedicoDatabase.getDatabase(getActivity());
+
     }
 
     @Override
@@ -72,6 +67,7 @@ public class doctors extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 
         Button btnAddNewDoctor = getActivity().findViewById(R.id.btn_new_doctor);
+        fillDoctors();
 
         btnAddNewDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +76,18 @@ public class doctors extends Fragment {
                 NavHostFragment.findNavController(doctors.this).navigate(R.id.action_page_doctors_to_add_doctors);
             }
         });
+    }
+
+    private void fillDoctors(){
+
+        GridView gridDoctors = getActivity().findViewById(R.id.gridview_doctors);
+
+        List<Doctor> doctorsList = db.doctorDao().getAll();
+
+        doctorAdapter adapter = new doctorAdapter(getActivity(), doctorsList);
+
+        gridDoctors.setAdapter(adapter);
+
     }
 
 }
