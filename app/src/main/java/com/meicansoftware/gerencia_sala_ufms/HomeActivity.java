73e3c2f46a9bef56f8d531e795519c8923508d56
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-17 00:02:47 -0300

    feat: adicionado home activity

---
+++ app/src/main/java/com/meicansoftware/gerencia_sala_ufms/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.meicansoftware.gerencia_sala_ufms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
