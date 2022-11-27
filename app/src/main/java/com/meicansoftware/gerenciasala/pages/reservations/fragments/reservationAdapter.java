4115c39b8f774f774b705096514d23a868b031e1
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-27 18:22:03 -0300

    feat: added feature to show course and events reservations

--- app/src/main/java/com/meicansoftware/gerenciasala/pages/reservations/fragments/reservationAdapter.java
+++ app/src/main/java/com/meicansoftware/gerenciasala/pages/reservations/fragments/reservationAdapter.java
@@ -1,7 +1,8 @@
-package com.meicansoftware.gerenciasala.pages.home.fragments;
+package com.meicansoftware.gerenciasala.pages.reservations.fragments;
 
 import android.content.Context;
-import android.os.Bundle;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.meicansoftware.gerenciasala.R;
 
@@ -19,11 +19,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class roomAdapter extends BaseAdapter {
+import java.text.ParseException;
+import java.util.Date;
+
+public class reservationAdapter extends BaseAdapter {
     private JSONArray rooms;
     private LayoutInflater mInflater;
 
-    public roomAdapter(Context context, JSONArray rooms){
+    public reservationAdapter(Context context, JSONArray rooms){
         this.rooms = rooms;
         mInflater = LayoutInflater.from(context);
     }
@@ -46,24 +49,78 @@ public class roomAdapter extends BaseAdapter {
     }
 
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View listRoomsView = convertView;
-        if (listRoomsView == null) {
+        View listView = convertView;
+        if (listView == null) {
             // Layout Inflater inflates each item to be displayed in GridView.
-            listRoomsView = mInflater.inflate(R.layout.gridview_layout_rooms, null);
+            listView = mInflater.inflate(R.layout.gridview_layout_reservations, null);
         }
-         JSONObject room_object = getItem(position);
+         JSONObject reservation_object = getItem(position);
 
-        TextView txt_name = listRoomsView.findViewById(R.id.txt_nome_sala);
-        TextView txt_obs = listRoomsView.findViewById(R.id.txt_obs_sala);
+
+        TextView txt_date_inicio = listView.findViewById(R.id.txt_data_inicio);
+        TextView txt_date_fim = listView.findViewById(R.id.txt_data_fim);
+        TextView txt_hora_inicio = listView.findViewById(R.id.txt_hora_inicio);
+        TextView txt_hora_fim = listView.findViewById(R.id.txt_hora_fim);
+        TextView txt_sala = listView.findViewById(R.id.txt_nome_sala);
+        TextView txt_nome_responsavel = listView.findViewById(R.id.txt_responsavel_agendamento);
+        TextView txt_course_event = listView.findViewById(R.id.txt_course_event);
+        JSONObject courseObject = null;
+        JSONObject userObject = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            DateFormat sourceFormat = new SimpleDateFormat("yyyy-MM-ddThh:MM:ss");
+            String dateAsString = "25/12/2010";
+            try {
+                Date date = sourceFormat.parse(dateAsString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        try {
+            courseObject = reservation_object.getJSONObject("course");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject eventObject = null;
+        try {
+            eventObject = reservation_object.getJSONObject("event");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            userObject = reservation_object.getJSONObject("user");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         try{
-            txt_name.setText(room_object.getString("nome_sala"));
-            txt_obs.setText(room_object.getString("observacao"));
+
+            txt_hora_inicio.setText(reservation_object.getString("hr_inicio_evento"));
+            txt_hora_fim.setText(reservation_object.getString("hr_fim_evento"));
+            txt_date_inicio.setText(reservation_object.getString("dt_inicio"));
+            txt_date_fim.setText(reservation_object.getString("dt_fim"));
+            txt_nome_responsavel.setText(userObject.getString("nome") + " " + userObject.getString("sobrenome"));
+
+            if(courseObject.length() > 0){
+                Log.d("courseObject", courseObject.toString());
+                txt_course_event.setText("D: " + courseObject.getString("nome") + " (" + courseObject.getString("lotacao_faculdade") + ")");
+                txt_date_inicio.setText(reservation_object.getString("dt_inicio"));
+                txt_date_fim.setText(reservation_object.getString("dt_fim"));
+
+            }
+            else if(eventObject.length() > 0){
+                txt_course_event.setText("E"  + eventObject.getString("nome") + " (" + eventObject.getString("lotacao_faculdade") + ")");
+
+            }else{
+                txt_course_event.setText("Sem título");
+            }
+
         }catch(Exception e){
             Log.d("e", e.getMessage());
         }
-
-        return listRoomsView;
+        return listView;
     }
 
 
--- app/src/main/java/com/meicansoftware/gerenciasala/pages/reservations/fragments/reservations.java
+++ app/src/main/java/com/meicansoftware/gerenciasala/pages/reservations/fragments/reservations.java
@@ -1,5 +1,7 @@
-package com.meicansoftware.gerenciasala.pages.events.reservations;
+package com.meicansoftware.gerenciasala.pages.reservations.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -7,13 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import com.meicansoftware.gerenciasala.R;
+import com.meicansoftware.gerenciasala.pages.home.fragments.roomAdapter;
 import com.meicansoftware.gerenciasala.services.LoginService;
+import com.meicansoftware.gerenciasala.services.ReservationService;
+import com.meicansoftware.gerenciasala.services.RoomService;
 import com.meicansoftware.gerenciasala.services.SignUpService;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,14 +54,55 @@ public class reservations extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        return inflater.inflate(R.layout.fragment_reservations, container, false);
     }
 
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        SharedPreferences userDetailsPreferences = this.getActivity().getSharedPreferences("users_detail", Context.MODE_PRIVATE);
+        String nomeUsuarioLogado = userDetailsPreferences.getString("nome", "");
+        String idUsuarioLogado = userDetailsPreferences.getString("id", "");
+        String sobrenomeUsuarioLogado = userDetailsPreferences.getString("sobrenome", "");
 
-    
+        fillReservations();
+
     }
 
+    public void fillReservations(){
+
+        GridView gridReservations = getActivity().findViewById(R.id.gridlayout_reservations_getAll);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String strReservations = null;
+
+        ReservationService reservationService = new ReservationService();
+        JSONArray jsonArray = null;
+        JSONObject obj = null;
+
+        try {
+            strReservations = reservationService.getReservations();
+            Log.d("strReservations", strReservations);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try{
+            jsonArray = new JSONArray(strReservations);
+        }catch(Exception e){
+            Log.e("Error", e.getMessage());
+        }
+
+        try{
+            obj = jsonArray.getJSONObject(0);
+            Log.d("e", obj.toString());
+        }catch(Exception e){
+            Log.e("Error", "Ocorreu um erro!");
+        }
+
+        reservationAdapter adapter = new reservationAdapter(getActivity(), jsonArray);
+        gridReservations.setAdapter(adapter);
+
+
+    }
 
 }
